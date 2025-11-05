38952f12390715bb80f3881ff2deb26e632f15ad
Angus McRitchie <angus@hobbiesdirect.com.au>
2025-11-05 16:12:41 +1100

    Slightly improve accuracy Lottery runCallback return type

--- src/Illuminate/Support/Lottery.php
+++ src/Illuminate/Support/Lottery.php
@@ -133,7 +133,7 @@ class Lottery
      * Run the winner or loser callback, randomly.
      *
      * @param  mixed  ...$args
-     * @return callable
+     * @return mixed
      */
     protected function runCallback(...$args)
     {
